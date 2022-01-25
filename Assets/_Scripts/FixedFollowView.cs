e4f12a75b26df57cf699996bdc94111aedde30c0
Wiloux <54811449+Wiloux@users.noreply.github.com>
2022-01-25 10:25:08 +0100

    4 Fixed Position Follow

--- Assets/_Scripts/FixedFollowView.cs
+++ Assets/_Scripts/FixedFollowView.cs
@@ -6,24 +6,61 @@ public class FixedFollowView : AView
 {
 
     public float roll;
-    public float fov;
+    private float yaw;
+    private float pitch;
+
+    [Range(90f,180f)]
+    public float fov = 90f;
+    public override CameraConfiguration GetConfiguration() { return new CameraConfiguration(yaw, pitch, roll, transform.position, fov); }
 
     public Transform target;
 
+    public GameObject centralPoint;
 
+    public float yawOffsetMax = 50;
+    public float pitchOffsetMax = 50;
 
     public float CalculateYaw(Transform target)
     {
-       Vector3 dir = (transform.position - target.position).normalized;
+        if (!target)
+            return 0;
 
-       return Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+        Vector3 dir = (target.position - transform.position).normalized;
+        return Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
     }
 
     public float CalculatePitch(Transform target)
     {
-        Vector3 dir = (transform.position - target.position).normalized;
+        if (!target)
+            return 0; 
 
+        Vector3 dir = (target.position - transform.position).normalized;
         return -Mathf.Asin(dir.y) * Mathf.Rad2Deg;
+    }
+
+    private void Update()
+    {
+      pitch = CalculatePitch(target);
+      yaw = CalculateYaw(target);
+        if (CameraController.Instance)
+        {
+            CameraController.Instance.UpdateActiveViewsAverageConfiguration();
+        }
+       
+    }
+    public override void OnDrawGizmos()
+    {
+        CameraConfiguration config = GetConfiguration();
+
+        if (!Camera.main || config == null) { return; }
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(config.pivot, 0.175f);
+        Vector3 position = config.GetPosition();
+        Gizmos.DrawLine(config.pivot, position);
+        Gizmos.matrix = Matrix4x4.TRS(position, config.GetRotation(), Vector3.one);
+        Gizmos.DrawFrustum(Vector3.zero, config.fov, 0.5f, 0f, Camera.main.aspect);
+        Gizmos.matrix = Matrix4x4.identity;
     }
 
 }

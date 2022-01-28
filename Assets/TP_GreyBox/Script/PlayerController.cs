5bfc5169529a24d82780543a972d911ea6381390
edwin-od <edwin.odeimi@lau.edu>
2022-01-28 11:21:03 +0100

    ..

--- Assets/TP_GreyBox/Script/PlayerController.cs
+++ Assets/TP_GreyBox/Script/PlayerController.cs
@@ -6,8 +6,6 @@ public class PlayerController : MonoBehaviour
 {
 	public float speed = 10.0f;
 
-	public float maxVelocity = 2;
-
 	Rigidbody _rigidbody = null;
 	protected bool IsActive { get; private set; }
 
@@ -17,12 +15,11 @@ public class PlayerController : MonoBehaviour
 	}
 
 	void FixedUpdate()
-    {
+	{
 		Vector3 direction = Vector3.zero;
 		direction += Input.GetAxisRaw("Horizontal") * Vector3.right;
 		direction += Input.GetAxisRaw("Vertical") * Vector3.forward;
 		direction.Normalize();
-
-		if (_rigidbody && _rigidbody.velocity.magnitude < maxVelocity) { _rigidbody.velocity += direction * speed * Time.deltaTime; }
+		_rigidbody.velocity = direction * speed + Vector3.up * _rigidbody.velocity.y;
 	}
 }
